bbd5ada9409934c39622088d3a4cb949cffcc5dc
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-07-22 12:51:38 +0500

    Add simple PHP script

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<html>
-    <body>
-        <h1>My Project</h1>
-</body>
-</html>
+<?php
+echo "<h1>Welcome to PHP</h1>";
+echo "<p>Today is " . date("l, F j, Y") . "</p>";
+echo "<p>Server time: " . date("h:i:s A") . "</p>";
+?>
