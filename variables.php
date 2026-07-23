75952c973dd8fafe3cb1ae059ca8849606e08b76
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-07-23 11:16:37 +0500

    Added variables in php

---
+++ variables.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Variables</title>
+</head>
+<body>
+
+<?php
+$name = "Umar";
+$age = 20;
+$city = "Karachi";
+$course = "PHP";
+?>
+
+<h1>My Info</h1>
+<p>Name: <?php echo $name; ?></p>
+<p>Age: <?php echo $age; ?></p>
+<p>City: <?php echo $city; ?></p>
+<p>Course: <?php echo $course; ?></p>
+
+</body>
+</html>
